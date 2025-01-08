e5a64b2375628cde4694bba79873dfaed23f0ffe
Aidan Simkin <shrimpkin10@gmail.com>
2025-01-08 16:59:44 -0600

    soldier spam

--- java/src/V1/Units/Tower.java
+++ java/src/V1/Units/Tower.java
@@ -6,11 +6,20 @@ import V1.*;
 
 public class Tower extends Globals {
     public static int turn_num = 0;
+    public static int num_built_soldier = 0;
 
     public static void run() throws GameActionException {
         turn_num++;
         rc.setIndicatorString(rc.getPaint() + " ");
         
+        //upgrades both our towers on round one,
+        //build order type stuff, should be experimented with
+        if(rc.getRoundNum() == 1) {
+            if(rc.canUpgradeTower(rc.getLocation())) {
+                rc.upgradeTower(rc.getLocation());
+            }
+        }
+
         if(rc.getType() == UnitType.LEVEL_ONE_PAINT_TOWER) {
             run_paint();
         }
@@ -23,6 +32,9 @@ public class Tower extends Globals {
     public static void attack() throws GameActionException{
         RobotInfo[] robotInfo = rc.senseNearbyRobots(-1, opponentTeam);
         for(RobotInfo robot : robotInfo) {
+            if(rc.canAttack(robot.getLocation())) {
+                rc.attack(robot.getLocation());
+            }
             if(rc.canAttack(robot.getLocation())) {
                 rc.attack(robot.getLocation());
             }
@@ -43,11 +55,13 @@ public class Tower extends Globals {
      */
     public static void run_paint() throws GameActionException {
         //building a solider to do some paint testing
-        if(turn_num == 1) {
+        if(num_built_soldier * 100 < rc.getRoundNum()) {
             if(rc.canBuildRobot(UnitType.SOLDIER, rc.getLocation().add(Direction.EAST))) {
+                num_built_soldier++;
                 rc.buildRobot(UnitType.SOLDIER, rc.getLocation().add(Direction.EAST));
             }
         }
+        
         if(rc.canBuildRobot(UnitType.SPLASHER, rc.getLocation().add(Direction.WEST))) {
             rc.buildRobot(UnitType.SPLASHER, rc.getLocation().add(Direction.WEST));
         }
