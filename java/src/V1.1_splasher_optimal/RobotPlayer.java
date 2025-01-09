860ac5a48749c3a7b4dc61389210c651b2bdc6ba
Emily Yao <emily2004.yao@gmail.com>
2025-01-09 12:34:12 -0600

    wip splashing optimal squares

---
+++ java/src/V1.1_splasher_optimal/RobotPlayer.java
@@ -0,0 +1,38 @@
+package V1;
+
+import V1.Units.*;
+import battlecode.common.*;
+
+public class RobotPlayer {
+    static int turnCount = 0;
+
+    public static void run(RobotController rc) throws GameActionException {
+        while(true) {
+            turnCount += 1;
+            if(turnCount == 1) {
+                init(rc);
+            }
+
+            try {
+                switch (rc.getType()){
+                    case SOLDIER: Soldier.run(); break;
+                    case MOPPER:  Unit.wander(); break;
+                    case SPLASHER: Splasher.run(); break;
+                    default: 
+                        Tower.run(); 
+                        break;
+                }
+            } catch(GameActionException e) {
+                rc.setIndicatorString("exception");
+                e.printStackTrace();
+            } finally {
+                Clock.yield();
+            }
+        }
+    }   
+
+    public static void init(RobotController rc) {
+        Unit.init(rc); // this doesn't do anything
+        BellmanFordNavigator.init(rc);
+    }
+}
---
+++ java/src/V1.1_splasher_optimal/Units/Mopper.java
@@ -0,0 +1,10 @@
+package V1.Units;
+
+import battlecode.common.RobotController;
+import V1.*;
+
+public class Mopper extends Globals {
+    public static void run(RobotController rc) {
+        
+    }
+}
