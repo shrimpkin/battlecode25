6ba1d6d28169430d088013b15a5337e723529ba0
Aidan Simkin <shrimpkin10@gmail.com>
2025-01-09 20:14:26 -0600

    seeking soldiers

--- java/src/V3/Units/Soldier.java
+++ java/src/V3/Units/Soldier.java
@@ -1,6 +1,6 @@
 package V3.Units;
 
-import java.util.Map;
+import java.util.Set;
 
 import V3.*;
 import battlecode.common.*;
@@ -8,21 +8,31 @@ import battlecode.common.*;
 public class Soldier extends Unit {
     static MapLocation[] targets = new MapLocation[3];
     static Boolean[] valid_targets = {true, true, true};
-    static MapLocation tower_location = null;
+    
+    static MapLocation[] tower_locations = new MapLocation[3];
+    static Boolean[] dead_tower = {false, false, false};
 
     public static void run() throws GameActionException {
         indicator = "";
 
+        notice_towers();
         get_rush_targets();
         update_rush_targets();
         move();
         attack();
 
+        for(int i = 0; i < tower_locations.length; i++) {
+            if(tower_locations[i] != null && !dead_tower[i]) {
+                indicator += tower_locations[i].toString() + ". ";
+            }
+        }
+
         for(int i = 0; i < 3; i++) {
-            if(valid_targets[i]) {
+            if(targets[i] != null && valid_targets[i]) {
                 indicator += targets[i].toString() + ", ";
             }
         }
+
         rc.setIndicatorString(indicator);
     }
 
@@ -42,6 +52,44 @@ public class Soldier extends Unit {
                 targets[1] = mirr;
                 targets[2] = hort;
             }
+        }        
+    }
+
+    public static void notice_towers() throws GameActionException {
+        //sensing new towers
+        RobotInfo[] robots = rc.senseNearbyRobots(-1, opponentTeam);
+        for(RobotInfo robot : robots) {
+            if(robot.getType() == UnitType.LEVEL_ONE_MONEY_TOWER || robot.getType() == UnitType.LEVEL_ONE_PAINT_TOWER) {
+                for(int i = 0; i < tower_locations.length; i++) {
+                    if(tower_locations[i] == null) {
+
+                        boolean should_update = true;
+                        for(int j = 0; j < tower_locations.length; j++) {
+                            if(tower_locations[j] != null && tower_locations[j] == robot.getLocation()) {
+                                should_update = false;
+                            } 
+                        }
+
+                        if(should_update) {
+                            tower_locations[i] = robot.getLocation();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        //updating dead towers
+        MapLocation[] ruins = rc.senseNearbyRuins(-1);
+        for(MapLocation loc : ruins) {
+            indicator += "ruin: " + loc.toString();
+            for(int i = 0; i < tower_locations.length; i++) {
+                
+                if(tower_locations[i] != null && tower_locations[i].equals(loc) && rc.senseRobotAtLocation(loc) == null) {
+                    System.out.println("Killed tower!");
+                    dead_tower[i] = true;
+                }
+            }
         }
     }
 
@@ -58,17 +106,22 @@ public class Soldier extends Unit {
     }
 
     public static void move() throws GameActionException {
-
-        if(targets[0] == null) {
-            wander();
-            return;
+        for(int i = 0; i < tower_locations.length; i++) {
+            if(tower_locations[i] != null && !dead_tower[i]) {
+                Navigator.moveTo(targets[i], true);
+                return;
+            }
+            
         }
 
-        for(int i = 0; i < 3; i++) {
+        for(int i = 0; i < targets.length; i++) {
             if(!valid_targets[i]) continue;
 
             Navigator.moveTo(targets[i], true);
+            return;
         }
+
+        wander();
     }
 
     public static void attack() throws GameActionException {
