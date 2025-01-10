67e8675cba9fb848adbd27a098a92c09cc2cb046
Emily Yao <emily2004.yao@gmail.com>
2025-01-09 21:03:28 -0600

    v1splasher offshoot for nav fixing

---
+++ java/src/V1_2/Navigator.java
@@ -0,0 +1,61 @@
+package V1_2;
+
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.Direction;
+
+public class Navigator extends Globals {
+    private static MapLocation currentTarget;
+
+    private static int minDistanceToTarget;
+    private static int roundsSinceMovingCloserToTarget;
+
+    public static void moveTo(MapLocation target) throws GameActionException {
+        MapLocation myLocation = rc.getLocation();
+
+        // draw line to show where its going
+        // rc.setIndicatorLine(rc.getLocation(), target, 255, 0, 0);
+
+        if (myLocation.equals(target)) {
+            return;
+        }
+
+        if (currentTarget == null || !currentTarget.equals(target)) {
+            reset();
+        }
+
+        currentTarget = target;
+
+        //causes it to not move idk
+        // MapLocation nextLocation = myLocation.add(myLocation.directionTo(target));
+        // if (rc.canSenseLocation(nextLocation)) {
+        //     return;
+        // }
+
+        int distanceToTarget = myLocation.distanceSquaredTo(target);
+        if (distanceToTarget < minDistanceToTarget) {
+            minDistanceToTarget = distanceToTarget;
+            roundsSinceMovingCloserToTarget = 0;
+        } else {
+            roundsSinceMovingCloserToTarget++;
+        }
+
+        Direction bellmanFordDirection = BellmanFordNavigator.getBestDirection(target);
+        if (bellmanFordDirection != null) {
+            if (rc.canMove(bellmanFordDirection)) {
+                rc.move(bellmanFordDirection);
+            }
+            return;
+        }
+        
+        if (!rc.isMovementReady()) {
+            return;
+        }
+    }
+
+    public static void reset() {
+        currentTarget = null;
+        minDistanceToTarget = Integer.MAX_VALUE;
+        roundsSinceMovingCloserToTarget = 0;
+    }
+}
---
+++ java/src/V1_2/Units/Splasher.java
@@ -0,0 +1,87 @@
+package V1_2.Units;
+
+import V1_2.*;
+import battlecode.common.*;
+
+public class Splasher extends Globals {
+    // how many tiles need to be enemy tiles to splash
+    private static final int minNumEnemySquares = 2;
+    // max number of ally tiles that will be overridden in a splash
+    private static final int maxNumAllySquares = 4;
+
+    public static void run() throws GameActionException {
+        Unit.update_paint_tower_loc();
+
+        splash();
+        refill();    
+        
+        Unit.wander();
+    }
+
+    public static void splash() throws GameActionException {
+        MapLocation currLoc = rc.getLocation();
+        MapLocation optLoc = getClosestOptimalLoc(currLoc);
+
+        // try to splash now
+        if (currLoc.equals(optLoc) && (canOptimalSplash() || canSplashEnemies())) {
+            if (rc.canAttack(currLoc)) {
+                rc.attack(currLoc);
+            }     
+            return;
+        }
+
+        // move and try to splash
+        Navigator.moveTo(optLoc);
+        if (canOptimalSplash() || canSplashEnemies()) {
+            if (rc.canAttack(optLoc)) {
+                rc.attack(optLoc);
+            }     
+        }
+    }
+
+    public static void refill() throws GameActionException {
+        if (rc.getPaint() >= 50) {
+            return;
+        }
+
+        Navigator.moveTo(Unit.paint_tower);
+        Unit.acquire_paint();
+    }
+
+    public static boolean canOptimalSplash() throws GameActionException {
+        int numAllySquares = 0;
+        for (MapInfo tile : rc.senseNearbyMapInfos(4)) {
+            if (tile.isWall()) {
+                continue;
+            }
+            PaintType paintType = tile.getPaint();
+            if (paintType == PaintType.ALLY_PRIMARY || paintType == PaintType.ALLY_SECONDARY) {
+                numAllySquares++;
+            }
+        }
+        return numAllySquares < maxNumAllySquares;
+    }
+
+    public static boolean canSplashEnemies() throws GameActionException {
+        int numEnemySquares = 0;
+        for (MapInfo tile : rc.senseNearbyMapInfos(2)) {
+            if (tile.isWall()) {
+                continue;
+            }
+            PaintType paintType = tile.getPaint();
+            if (paintType == PaintType.ENEMY_PRIMARY || paintType == PaintType.ENEMY_SECONDARY) {
+                numEnemySquares++;
+            }
+        }
+        return numEnemySquares >= minNumEnemySquares;
+    }
+
+    public static MapLocation getClosestOptimalLoc(MapLocation currLoc) throws GameActionException {
+        int[] pt = {
+                (int) Math.round(Utils.mp[0][0] * currLoc.x + Utils.mp[0][1] * currLoc.y),
+                (int) Math.round(Utils.mp[1][0] * currLoc.x + Utils.mp[1][1] * currLoc.y),
+        };
+        int[] remapped = Utils.mvmul(Utils.rmap, pt);
+        return new MapLocation(remapped[0], remapped[1]);
+    }
+}
