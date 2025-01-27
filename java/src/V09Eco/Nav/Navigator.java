b10b48390ce07f7edefa4b1fda02e7109e3ba4e5
Aidan Simkin <shrimpkin10@gmail.com>
2025-01-26 20:44:35 -0600

    why mark much when none do

---
+++ java/src/V09Eco/Nav/Navigator.java
@@ -0,0 +1,125 @@
+package V09Eco.Nav;
+
+import V09Eco.Globals;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+
+
+public class Navigator extends Globals {
+    static BugPath path = new BugPath();
+
+    public static void moveTo(MapLocation target) throws GameActionException {
+        path.moveTo(target);
+    }
+
+    public static void moveTo(MapLocation target, boolean allyOnly) throws GameActionException {
+        path.moveTo(target, allyOnly);
+    }
+}
+
+// package V09.Nav;
+
+// import V09.Globals;
+// import battlecode.common.Direction;
+// import battlecode.common.GameActionException;
+// import battlecode.common.MapLocation;
+
+
+// public class Navigator extends Globals {
+//     private static MapLocation currentTarget;
+
+//     private static int minDistanceToTarget;
+//     private static int roundsSinceMovingCloserToTarget;
+
+//     public static void moveTo(MapLocation target) throws GameActionException {
+//         MapLocation myLocation = rc.getLocation();
+//         if(rc.onTheMap(target)) rc.setIndicatorLine(rc.getLocation(), target, 255, 0, 0);
+
+//         if (myLocation.equals(target)) {
+//             return;
+//         }
+
+//         if (currentTarget == null || !currentTarget.equals(target)) {
+//             reset();
+//         }
+
+//         currentTarget = target;
+
+//         int distanceToTarget = myLocation.distanceSquaredTo(target);
+//         if (distanceToTarget < minDistanceToTarget) {
+//             minDistanceToTarget = distanceToTarget;
+//             roundsSinceMovingCloserToTarget = 0;
+//         } else {
+//             roundsSinceMovingCloserToTarget++;
+//         }
+
+//         if (roundsSinceMovingCloserToTarget < 3) {
+//             Direction bellmanFordDirection = BellmanFordNavigator.getBestDirection(target);
+//             if (bellmanFordDirection != null) {
+                
+//                 if (rc.canMove(bellmanFordDirection)) {
+//                     rc.move(bellmanFordDirection);
+//                 }
+
+//                 return;
+//             } 
+//         } 
+
+//         if (!rc.isMovementReady()) {
+//             return;
+//         }
+
+//         BugNavigator.move(target);
+//     }
+    
+//     /**
+//      * @param paintless true if the robot shouldn't move onto any squares that cost paint
+//      */
+//     public static void moveTo(MapLocation target, boolean paintless) throws GameActionException {
+//         MapLocation myLocation = rc.getLocation();
+//         rc.setIndicatorLine(rc.getLocation(), target, 255, 0, 0);
+
+//         if (myLocation.equals(target)) {
+//             return;
+//         }
+
+//         if (currentTarget == null || !currentTarget.equals(target)) {
+//             reset();
+//         }
+
+//         currentTarget = target;
+
+//         int distanceToTarget = myLocation.distanceSquaredTo(target);
+//         if (distanceToTarget < minDistanceToTarget) {
+//             minDistanceToTarget = distanceToTarget;
+//             roundsSinceMovingCloserToTarget = 0;
+//         } else {
+//             roundsSinceMovingCloserToTarget++;
+//         }
+
+//         if (roundsSinceMovingCloserToTarget < 3) {
+//             Direction bellmanFordDirection = PaintlessPath.getBestDirection(target);
+//             if (bellmanFordDirection != null) {
+                
+//                 if (rc.canMove(bellmanFordDirection)) {
+//                     rc.move(bellmanFordDirection);
+//                 }
+
+//                 return;
+//             } 
+//         } 
+
+//         if (!rc.isMovementReady()) {
+//             return;
+//         }
+
+//         BugNavigator.move(target);
+//     }
+
+//     public static void reset() {
+//         currentTarget = null;
+
+//         minDistanceToTarget = Integer.MAX_VALUE;
+//         roundsSinceMovingCloserToTarget = 0;
+//     }
+// }
---
+++ java/src/V09Micro/Tools/CommType.java
@@ -0,0 +1,21 @@
+package V09Micro.Tools;
+
+public enum CommType {
+    /// tower->robot: need defenders to help current tower defend from attackers
+    WantDefenders,     // NEVER USED
+    /// tower->robot: target enemies in tower vicinity
+    TargetEnemy,       // no senders
+    /// tower->both : have a frontline that we want backline units to help out with
+    ReinforceFront,
+    /// tower->tower: comm telling each other what kinda tower they are, and spread news about paint towers
+    CommunicateType,   // NEVER USED
+    /// tower->robot: tell a robot the nearest paint tower to this one
+    NearbyPaintTower,  // NEVER USED
+    /// robot->tower: ask for the closest paint tower this one knows of
+    RequestPaintTower, // NEVER USED
+    
+    /// tower->tower: request PAINT TOWERS for soldiers for rebuilding
+    RequestSoldiers,   // NEVER USED
+    /// tower->robot: provides a location to rebuild at
+    RebuildTower,      // NEVER USED
+}
